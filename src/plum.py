51adaad1fd7d6ff7f582167d3c0e92bebe4d3bd9
Aditeya <aditeya.baral@gmail.com>
2022-02-15 19:20:12 +0530

    Add token selector model

--- src/plum.py
+++ src/plum.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from token_selector import TokenSelector
 from transformers import AutoModelForTokenClassification , AutoTokenizer
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast, RobertaTokenizerFast, XLMRobertaTokenizerFast
 
@@ -11,9 +12,10 @@ class PLUM(nn.Module):
 
         self.use_gpt2 = use_gpt2
         self.freeze_gpt2 = freeze_gpt2
-        if self.use_gpt2:
+        self.selector_type = selector_type
+        if self.use_gpt2 or self.selector_type == 'context':
             self.gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
-            self.gpt2_tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
+            self.gpt2_tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', add_prefix_space=True)
             if self.freeze_gpt2 != 'none':
                 for param in self.gpt2_model.parameters():
                     param.requires_grad = False
@@ -21,9 +23,8 @@ class PLUM(nn.Module):
                     for param in self.gpt2_model.lm_head.parameters():        
                         param.requires_grad = True
 
-        self.selector_type = selector_type
         if selector_type == 'context':
-            pass
+            self.selector = TokenSelector(self.gpt2_model, self.gpt2_tokenizer, self.device).to(self.device)
         else:
             self.maple = AutoModelForTokenClassification.from_pretrained(selector_type)
             self.maple_tokenizer = self.get_tokenizer(selector_type)
@@ -83,17 +84,13 @@ class PLUM(nn.Module):
 
 
     def forward(self, tokens, labels):
+        output_sequences = list()
         if self.selector_type != 'context':
             token_encodings, attention_masks, label_encodings = self.get_encodings_and_labels(tokens, labels)
             outputs = self.maple(input_ids=token_encodings, attention_mask=attention_masks, labels=label_encodings)
             loss_m, logits = outputs[:2]
             logits = logits.softmax(dim=2).argmax(dim=2)
-            
-        else:
-            pass
 
-        if self.use_gpt2:
-            loss_lm = list()
             for i in range(token_encodings.shape[0]):
                 input_sequence = token_encodings[i]
                 logit_sequence = logits[i]
@@ -103,16 +100,30 @@ class PLUM(nn.Module):
                     if attention_mask[j] and logit_sequence[j]:
                         output_sequence.append(input_sequence[j])
                 output_sequence = self.maple_tokenizer.decode(output_sequence).strip()
-
                 if not output_sequence: # if no words are chosen, take entire input sequence
                     output_sequence = ' '.join(tokens[i])
+                output_sequences.append(output_sequence)
+            
+        else:
+            loss_m = list()
+            for idx, t in enumerate(tokens):
+                chosen_words, _, loss = self.selector(t)
+                if chosen_words:
+                    output_sequence = ' '.join(chosen_words)
+                else:
+                    output_sequence = ' '.join(t)
+                output_sequences.append(output_sequence)
+                loss_m.append(loss)
+            loss_m = torch.stack(loss_m).mean()
 
+        if self.use_gpt2:
+            loss_lm = list()
+            for output_sequence in output_sequences:
                 input_sequence_ids = self.gpt2_tokenizer.encode(output_sequence)
                 input_sequence_ids = torch.tensor(input_sequence_ids).to(self.device)
                 loss = self.gpt2_model(input_ids=input_sequence_ids, labels=input_sequence_ids).loss
                 loss = torch.exp(loss)
                 loss_lm.append(loss)
-            
             loss_lm = torch.stack(loss_lm).mean()
         else:
             loss_lm = 0
